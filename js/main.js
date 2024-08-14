e59cd3458298c468787db7072e27a9bdd52bc1e9
KaewTori <thonybum@gmail.com>
2024-08-15 01:51:15 +0700

    update text change

--- js/main.js
+++ js/main.js
@@ -18,28 +18,61 @@ function startexhibition() {
 
 function nextAnimalProcess() {
     $("body").removeClass("animal-animations-on"),
-        turnoffslideshow(),
+        // turnoffslideshow(),
         prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
         newAnimal = prevAnimal + 1,
-        30 == newAnimal && (newAnimal = 0),
-        nextorprevanimal(),
-        animationstatequestion()
+        2 == newAnimal && (newAnimal = 0),
+        nextorprevanimal()
+    // animationstatequestion()
 }
 
 function previousAnimalProcess() {
     $("body").removeClass("animal-animations-on"),
-        turnoffslideshow(),
+        // turnoffslideshow(),
         prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
         newAnimal = prevAnimal - 1,
-        -1 == newAnimal && (newAnimal = 29),
-        nextorprevanimal(),
-        animationstatequestion()
+        -1 == newAnimal && (newAnimal = 0),
+        nextorprevanimal()
+    // animationstatequestion()
 }
 
+function nextorprevanimal() {
+    setTimeout(function () {
+        $(".shadow").removeClass("inactive")
+    }, 200),
+        prevAnimal > newAnimal ? ($(".wrap").addClass("right-to-left"),
+            $(".wrap.left-to-right").removeClass("left-to-right")) : ($(".wrap").addClass("left-to-right"),
+                $(".wrap.right-to-left").removeClass("right-to-left")),
+        $("#animalchanger").attr("class", animalList[newAnimal]),
+        $(".next .popout").attr("popanimal", animalList[newAnimal]),
+        $(".prev .popout").attr("popanimal", animalList[newAnimal]),
+        $(".popout").addClass("text-change"),
+        $(".animalinfo").addClass("text-change"),
+        // $(".content-wrap .content-info .infos ul li:nth-child(1) span").html(animalScientificName[newAnimal]),
+        // $(".content-wrap .content-info .infos ul li:nth-child(3) span").html(animalRange[newAnimal]),
+        // $(".content-wrap .content-info .infos p:nth-child(3)").html(animalCopyOne[newAnimal]),
+        // $(".content-wrap .content-info .infos p:nth-child(4)").html(animalCopyTwo[newAnimal]),
+        // $(".content-wrap .content-info .infos p:nth-child(5)").html(animalCopyThree[newAnimal]),
+        // $(".content-wrap .content-info .ctas li:nth-child(3)").html(animalCharity[newAnimal]),
+        setTimeout(function () {
+            $(".animalinfo h2").text(animalNames[newAnimal]),
+                $(".animalinfo li:nth-child(1) span").text(newAnimal + 1),
+                $(".popout").removeClass("text-change"),
+                $(".animalinfo").removeClass("text-change"),
+                0 == newAnimal ? ($(".prev .popout").text(animalNames[1]),
+                    $(".next .popout").text(animalNames[1])) : 1 == newAnimal ? ($(".prev .popout").text(animalNames[0]),
+                        $(".next .popout").text(animalNames[0])) : ($(".prev .popout").text(animalNames[newAnimal - 1]),
+                            $(".next .popout").text(animalNames[newAnimal + 1]))
+        }, 150)
+}
+
+newAnimal = 0;
 $(document).ready(function () {
 
     $(".start-btn").on("click", function () {
-        startexhibition()
+        startexhibition(),
+        prevAnimal = 2,
+        nextorprevanimal()
     }),
         $(".thobbing").on("click", function () {
             overlayContent = ".overlay .detail",
@@ -49,35 +82,54 @@ $(document).ready(function () {
             overlayContent = ".overlay .all-animal-stg",
                 overlayprocess()
         }),
-        $("footer .aboutme a").on("click", function () {
-            overlayContent = ".overlay .author",
-                overlayprocess()
-        }),
         $(".overlay .close").on("click", function () {
             $(".overlay").toggleClass("active"),
                 setTimeout(function () {
                     $(".overlay div").removeClass("active")
                 }, 500)
         }),
-        $(".main-nav li:nth-child(3)").on("click", function () {
-            // $(".main-nav li:nth-child(3).muted").length ? (soundManager.unmute(),
-            // $(this).removeClass("muted"),
-            // $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
-            // setTimeout(function() {
-            //     $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
-            //     $(".main-nav li:nth-child(3) .popout").text("Get that racket off")
-            // }, 150)) : (soundManager.mute(),
-            // $(this).addClass("muted"),
-            // $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
-            // setTimeout(function() {
-            //     $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
-            //     $(".main-nav li:nth-child(3) .popout").text("Beautify with sound")
-            // }, 150))
-            $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
+        $("footer .aboutme a").on("click", function () {
+            overlayContent = ".overlay .author",
+                overlayprocess()
+        }),
+        $(".next").on("click", function () {
+            nextAnimalProcess()
+        }),
+        $(".prev").on("click", function () {
+            previousAnimalProcess()
+        }),
+        // $(".menu-nav li:nth-child(3)").on("click", function () {
+        //     $(".menu-nav li:nth-child(3).muted").length ? (soundManager.unmute(),
+        //         $(this).removeClass("muted"),
+        //         $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
+        //         setTimeout(function () {
+        //             $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
+        //                 $(".menu-nav li:nth-child(3) .popout").text("Get that racket off")
+        //         }, 150)) : (soundManager.mute(),
+        //             $(this).addClass("muted"),
+        //             $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
+        //             setTimeout(function () {
+        //                 $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
+        //                     $(".menu-nav li:nth-child(3) .popout").text("Beautify with sound")
+        //             }, 150))
+        // })
+        $(".menu-nav li:nth-child(3)").on("click", function () {
+            $(".menu-nav li:nth-child(3).muted").length ? (
+                $(this).removeClass("muted"),
+                $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
                 setTimeout(function () {
-                    $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
-                        $(".main-nav li:nth-child(3) .popout").text("Get that racket off")
+                    $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
+                        $(".menu-nav li:nth-child(3) .popout").text("Get that racket off"),
+                        $(".menu-nav li:nth-child(3) a").text("music On")
+                }, 150)) : (
+                $(this).addClass("muted"),
+                $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
+                setTimeout(function () {
+                    $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
+                        $(".menu-nav li:nth-child(3) .popout").text("Beautify with sound")
+                    $(".menu-nav li:nth-child(3) a").text("music Off")
                 }, 150)
+            )
         })
 
 })

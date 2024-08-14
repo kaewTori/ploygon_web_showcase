cd4e72ee0e6e9ded0e0aa04152b8ca5d1772779e
KaewTori <thonybum@gmail.com>
2024-08-15 01:51:17 +0700

    add slide show and dynamic text detail

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 function overlayprocess() {
     $(overlayContent).toggleClass("active"),
-        setTimeout(function() {
+
+        setTimeout(function () {
             $(".overlay").toggleClass("active")
         }, 10)
 }
 
 function startexhibition() {
-    setTimeout(function() {
+    setTimeout(function () {
         $(".start-screen").fadeOut(500)
     }, 400);
 }
@@ -19,49 +20,50 @@ function nextAnimalProcess() {
 }
 
 function previousAnimalProcess() {
-    $("body").removeClass("animal-animations-on"),
-        prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
-        newAnimal = prevAnimal - 1, -1 == newAnimal && (newAnimal = 1),
+    prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
+        newAnimal = prevAnimal - 1,
+        -1 == newAnimal && (newAnimal = 1),
         nextorprevanimal()
 }
 
 function sizeshards() {
-    $(".stg-exhibi").each(function() {
-            var t = .99 * $(window).width(),
-                a = .7 * t,
-                n = -0.2 * (a / 2);
-            $(this).each(function() {
-                $(this).css({
-                    width: t,
-                    height: a,
-                    "margin-top": n,
-                })
+    $(".stg-exhibi").each(function () {
+        var t = .99 * $(window).width(),
+            a = .7 * t,
+            n = -0.2 * (a / 2);
+        $(this).each(function () {
+            $(this).css({
+                width: t,
+                height: a,
+                "margin-top": n,
             })
-        }),
+        })
+    }),
         $(window).width() < 700 || $("html.touch").length || $(".nowebkitbrowser").length ? $("html").addClass("small-nav") : $("html.webkitbrowser").length && $("html").removeClass("small-nav")
 }
 
 function nextorprevanimal() {
-    setTimeout(function() {
-            $(".shadow").removeClass("inactive")
-        }, 200),
-        prevAnimal > newAnimal ? ($(".wrap").addClass("right-to-left"),
-            $(".wrap.left-to-right").removeClass("left-to-right")) : ($(".wrap").addClass("left-to-right"),
-            $(".wrap.right-to-left").removeClass("right-to-left")),
-        $("#animalchanger").attr("class", animalList[newAnimal]),
+    $("#animalchanger").attr("class", animalList[newAnimal]),
         $(".next .popout").attr("popanimal", animalList[newAnimal]),
         $(".prev .popout").attr("popanimal", animalList[newAnimal]),
         $(".popout").addClass("text-change"),
         $(".animalinfo").addClass("text-change"),
-        setTimeout(function() {
+
+        setTimeout(function () {
             $(".animalinfo h2").text(animalNames[newAnimal]),
                 $(".animalinfo li:nth-child(1) span").text(newAnimal + 1),
                 $(".popout").removeClass("text-change"),
                 $(".animalinfo").removeClass("text-change"),
+
+                $(".overlay .detail h2").html(animalScientificName[newAnimal]),
+                $(".overlay .detail p:nth-child(2)").html(animalDetail1[newAnimal]),
+                $(".overlay .detail p:nth-child(3)").html(animalDetail2[newAnimal]),
+                $(".overlay .detail p:nth-child(4)").html(animalDetail3[newAnimal]),
+
                 0 == newAnimal ? ($(".prev .popout").text(animalNames[1]),
                     $(".next .popout").text(animalNames[1])) : 1 == newAnimal ? ($(".prev .popout").text(animalNames[0]),
-                    $(".next .popout").text(animalNames[0])) : ($(".prev .popout").text(animalNames[newAnimal - 1]),
-                    $(".next .popout").text(animalNames[newAnimal + 1]))
+                        $(".next .popout").text(animalNames[0])) : ($(".prev .popout").text(animalNames[newAnimal - 1]),
+                            $(".next .popout").text(animalNames[newAnimal + 1]))
         }, 150)
 }
 
@@ -73,8 +75,6 @@ function updateText(text) {
         return `<span>` + letter + `</span>`;
     }).join("");
 
-    console.log("test " + h1.innerHTML);
-
     Array.from(h1.children).forEach((span, index) => {
         setTimeout(() => {
             span.classList.add("wavy");
@@ -82,51 +82,94 @@ function updateText(text) {
     });
 }
 
+function startslideshow() {
+    $("body").addClass("slideshow-on"),
+    $(".menu-nav li:nth-child(2) a").addClass("stop"),
+    $(".menu-nav li:nth-child(2) .popout").addClass("text-change"),
+    setTimeout(function() {
+        $(".menu-nav li:nth-child(2) .popout").removeClass("text-change"),
+        $(".menu-nav li:nth-child(2) .popout").text("stop")
+    }, 150),
+    slideshowvar += 1,
+    slideshowquery()
+}
+
+function turnoffslideshow() {
+    slideshowvar += 1,
+    $(".menu-nav li:nth-child(2) a").removeClass("stop"),
+    $(".menu-nav li:nth-child(2) .popout").addClass("text-change"),
+    setTimeout(function() {
+        $(".menu-nav li:nth-child(2) .popout").removeClass("text-change"),
+        $(".menu-nav li:nth-child(2) .popout").text("Cycle Through")
+    }, 150),
+    $(".slideshow-on").removeClass("slideshow-on")
+}
+
+function slideshowquery() {
+    randomAnimal(),
+    slideshowvar += 1;
+    var t = slideshowvar;
+    setTimeout(function() {
+        t == slideshowvar && slideshowquery()
+    }, 2900)
+}
+
+function randomAnimal() {
+    newAnimal = Math.floor(Math.random() * finishedAnimals),
+    console.log(newAnimal),
+    nextorprevanimal()
+}
+
 newAnimal = 0;
-$(document).ready(function() {
+slideshowvar = 0;
+finishedAnimals = 2;
+$(document).ready(function () {
     sizeshards()
     updateText("NO TALES IN the sea NO SEE")
-    $(".start-btn").on("click", function() {
-            startexhibition(),
-                prevAnimal = 2,
-                nextorprevanimal()
-        }),
-        $(".thobbing").on("click", function() {
+    $(".start-btn").on("click", function () {
+        startexhibition(),
+            prevAnimal = 2,
+            nextorprevanimal()
+    }),
+        $(".thobbing").on("click", function () {
             overlayContent = ".overlay .detail",
                 overlayprocess()
         }),
-        $(".all-animal").on("click", function() {
+        $(".all-animal").on("click", function () {
             overlayContent = ".overlay .all-animal-stg",
                 overlayprocess()
         }),
-        $(".overlay .close").on("click", function() {
+        $(".overlay .close").on("click", function () {
             $(".overlay").toggleClass("active"),
-                setTimeout(function() {
+                setTimeout(function () {
                     $(".overlay div").removeClass("active")
                 }, 500)
         }),
-        $("footer .aboutme a").on("click", function() {
+        $("footer .aboutme a").on("click", function () {
             overlayContent = ".overlay .author",
                 overlayprocess()
         }),
-        $(".next").on("click", function() {
+        $(".next").on("click", function () {
             nextAnimalProcess()
         }),
-        $(".prev").on("click", function() {
+        $(".prev").on("click", function () {
             previousAnimalProcess()
         }),
-        $(".menu-nav li:nth-child(3)").on("click", function() {
+        $(".menu-nav li:nth-child(2) a").on("click", function() {
+            $("body.slideshow-on").length ? turnoffslideshow() : startslideshow()
+        }),
+        $(".menu-nav li:nth-child(3)").on("click", function () {
             $(".menu-nav li:nth-child(3).muted").length ? (
                 $(this).removeClass("muted"),
                 $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
-                setTimeout(function() {
+                setTimeout(function () {
                     $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
                         $(".menu-nav li:nth-child(3) .popout").text("Get that racket off"),
                         $(".menu-nav li:nth-child(3) a").text("music On")
                 }, 150)) : (
                 $(this).addClass("muted"),
                 $(".menu-nav li:nth-child(3) .popout").addClass("text-change"),
-                setTimeout(function() {
+                setTimeout(function () {
                     $(".menu-nav li:nth-child(3) .popout").removeClass("text-change"),
                         $(".menu-nav li:nth-child(3) .popout").text("Beautify with sound")
                     $(".menu-nav li:nth-child(3) a").text("music Off")
@@ -135,6 +178,6 @@ $(document).ready(function() {
         }),
         sizeshards()
 })
-$(window).resize(function() {
+$(window).resize(function () {
     sizeshards()
 })
--- js/var.js
+++ js/var.js
@@ -1,2 +1,12 @@
 var animalList = ["vaquita", "penguin"]
 var animalNames = ["Vaquita", "African Penguin"]
+var animalScientificName = ["Phocoena sinus", "Strigops habroptilus"]
+var animalDetail1 = ["The greatest threat to the remaining vaquita is incidental death caused by fishing gear.",
+    "This glorious hornbill uplisted 3 levels from Near Threatened to <span>Critically endangered</span> in late 2015."
+]
+var animalDetail2 = ["Vaquitas are known to die in gillnets set for sharks, rays, mackerels and chano, as well as in <span>illegal and occasionally permitted</span> gillnet sets for an endangered <i>fish called Totoaba.</i> ",
+    "Its highly-prized casque is unique in that it is almost completely solid and <span>rich in ivory</span> &ndash; leading to excessive hunting in recent years which is set to increase, with China the largest consumer of the casque. "
+]
+var animalDetail3 = ["It is believed that about 30 vaquitas are <br/>lost to these threats each year.",
+    "Logging and agricultural conversion is contributing to the ominous pressure of hunting, and restoration is complicated further by the hornbill&rsquo;s <span>slow reproductive cycle</span>."
+]

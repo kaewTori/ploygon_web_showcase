9c4e3d48c161221d13079ae46714c2fc79280ac8
KaewTori <thonybum@gmail.com>
2024-08-15 01:51:15 +0700

    add hovver text button and and text in all animal page

--- js/main.js
+++ js/main.js
@@ -18,22 +18,22 @@ function startexhibition() {
 
 function nextAnimalProcess() {
     $("body").removeClass("animal-animations-on"),
-    turnoffslideshow(),
-    prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
-    newAnimal = prevAnimal + 1,
-    30 == newAnimal && (newAnimal = 0),
-    nextorprevanimal(),
-    animationstatequestion()
+        turnoffslideshow(),
+        prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
+        newAnimal = prevAnimal + 1,
+        30 == newAnimal && (newAnimal = 0),
+        nextorprevanimal(),
+        animationstatequestion()
 }
 
 function previousAnimalProcess() {
     $("body").removeClass("animal-animations-on"),
-    turnoffslideshow(),
-    prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
-    newAnimal = prevAnimal - 1,
-    -1 == newAnimal && (newAnimal = 29),
-    nextorprevanimal(),
-    animationstatequestion()
+        turnoffslideshow(),
+        prevAnimal = animalList.indexOf($("#animalchanger").attr("class")),
+        newAnimal = prevAnimal - 1,
+        -1 == newAnimal && (newAnimal = 29),
+        nextorprevanimal(),
+        animationstatequestion()
 }
 
 $(document).ready(function () {
@@ -58,6 +58,26 @@ $(document).ready(function () {
                 setTimeout(function () {
                     $(".overlay div").removeClass("active")
                 }, 500)
+        }),
+        $(".main-nav li:nth-child(3)").on("click", function () {
+            // $(".main-nav li:nth-child(3).muted").length ? (soundManager.unmute(),
+            // $(this).removeClass("muted"),
+            // $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
+            // setTimeout(function() {
+            //     $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
+            //     $(".main-nav li:nth-child(3) .popout").text("Get that racket off")
+            // }, 150)) : (soundManager.mute(),
+            // $(this).addClass("muted"),
+            // $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
+            // setTimeout(function() {
+            //     $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
+            //     $(".main-nav li:nth-child(3) .popout").text("Beautify with sound")
+            // }, 150))
+            $(".main-nav li:nth-child(3) .popout").addClass("text-change"),
+                setTimeout(function () {
+                    $(".main-nav li:nth-child(3) .popout").removeClass("text-change"),
+                        $(".main-nav li:nth-child(3) .popout").text("Get that racket off")
+                }, 150)
         })
 
 })
@@ -66,7 +86,7 @@ SOUNDS = {
     btn: $("<a>").attr("href", "#").attr("id", "toggle-mute").addClass("toggle-mute").text(""),
     looping: null,
     fading: null,
-    init: function() {
+    init: function () {
         soundManager.setup({
             url: "swf/",
             debugMode: !1,
@@ -76,102 +96,102 @@ SOUNDS = {
             flashVersion: 9,
             multiShot: !0,
             onready: SOUNDS.onSoundManagerReady,
-            ontimeout: function() {}
+            ontimeout: function () { }
         })
     },
-    onSoundManagerReady: function() {
+    onSoundManagerReady: function () {
         soundManager.createSound({
             id: "ambientloop",
             url: "audio/ambient-loop-piano.mp3",
             autoLoad: !0,
             multiShot: !0,
-            onload: function() {
+            onload: function () {
                 SOUNDS.playLoop("ambientloop", 44500)
             }
         }),
-        soundManager.createSound({
-            id: "hover",
-            url: "audio/hover_ui.mp3",
-            autoLoad: !0,
-            onload: function() {}
-        }),
-        soundManager.createSound({
-            id: "softhover",
-            url: "audio/hover_ui.mp3",
-            autoLoad: !0,
-            onload: function() {
-                this.setVolume(50)
-            }
-        }),
-        soundManager.createSound({
-            id: "smashpiano",
-            url: "audio/smash.mp3",
-            autoLoad: !0,
-            onload: function() {
-                this.setVolume(30)
-            }
-        })
+            soundManager.createSound({
+                id: "hover",
+                url: "audio/hover_ui.mp3",
+                autoLoad: !0,
+                onload: function () { }
+            }),
+            soundManager.createSound({
+                id: "softhover",
+                url: "audio/hover_ui.mp3",
+                autoLoad: !0,
+                onload: function () {
+                    this.setVolume(50)
+                }
+            }),
+            soundManager.createSound({
+                id: "smashpiano",
+                url: "audio/smash.mp3",
+                autoLoad: !0,
+                onload: function () {
+                    this.setVolume(30)
+                }
+            })
     },
-    play: function(t) {
+    play: function (t) {
         soundManager.play(t)
     },
-    playLoop: function(t, a) {
+    playLoop: function (t, a) {
         SOUNDS[t + "looping"] || (SOUNDS[t + "looping"] = !0,
-        SOUNDS.play(t),
-        SOUNDS["looping" + t] = setInterval(function() {
-            SOUNDS.play(t)
-        }, a))
+            SOUNDS.play(t),
+            SOUNDS["looping" + t] = setInterval(function () {
+                SOUNDS.play(t)
+            }, a))
     },
-    stopLoop: function(t) {
+    stopLoop: function (t) {
         clearInterval(SOUNDS["looping" + t]),
-        SOUNDS[t + "looping"] = !1
+            SOUNDS[t + "looping"] = !1
     },
-    setVolume: function(t, a) {
+    setVolume: function (t, a) {
         var e = soundManager.getSoundById(soundID);
         e.setVolume(a)
     },
-    fadeIn: function(t) {
+    fadeIn: function (t) {
         if ("out" != SOUNDS.fading) {
             var a = soundManager.getSoundById(t);
             if (a) {
                 var e = a.volume;
                 if (e >= 100)
                     return SOUNDS.fading = "",
-                    !1;
+                        !1;
                 SOUNDS.fading = "in",
-                a.setVolume(Math.min(100, e + 10)),
-                setTimeout(function() {
-                    SOUNDS.fadeIn(t)
-                }, 50)
+                    a.setVolume(Math.min(100, e + 10)),
+                    setTimeout(function () {
+                        SOUNDS.fadeIn(t)
+                    }, 50)
             }
         }
     },
-    fadeOut: function(t) {
+    fadeOut: function (t) {
         if ("in" != SOUNDS.fading) {
             var a = soundManager.getSoundById(t);
             if (a) {
                 var e = a.volume;
                 if (40 >= e)
                     return SOUNDS.fading = "",
-                    !1;
+                        !1;
                 SOUNDS.fading = "out",
-                a.setVolume(Math.max(0, e - 10)),
-                setTimeout(function() {
-                    SOUNDS.fadeOut(t)
-                }, 50)
+                    a.setVolume(Math.max(0, e - 10)),
+                    setTimeout(function () {
+                        SOUNDS.fadeOut(t)
+                    }, 50)
             }
         }
     },
-    toggleMute: function(t) {
+    toggleMute: function (t) {
         t.preventDefault(),
-        SOUNDS.btn.hasClass("muted") ? SOUNDS.unmute() : SOUNDS.mute()
+            SOUNDS.btn.hasClass("muted") ? SOUNDS.unmute() : SOUNDS.mute()
     },
-    mute: function(t) {
+    mute: function (t) {
         soundManager.mute(t),
-        SOUNDS.btn.addClass("muted")
+            SOUNDS.btn.addClass("muted")
     },
-    unmute: function(t) {
+    unmute: function (t) {
         soundManager.unmute(t),
-        SOUNDS.btn.removeClass("muted")
+            SOUNDS.btn.removeClass("muted")
     }
 };
